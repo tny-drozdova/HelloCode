305e81478d6707183659c6e28359b207ac5497bf
Tanya Drozdova <tny.drozdova@gmail.com>
2023-06-10 21:48:26 -0400

    Add Examples 002 Hello User

--- Examples001_HelloConsole/Program.cs
+++ Examples001_HelloConsole/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-// WE use slashes for the comments
-Console.WriteLine("2 Hello, World!");
+﻿Console.Write("Please, enter your name ");
+string username = Console.ReadLine()!;
+Console.Write("Privet, ");
+Console.Write(username);
+
